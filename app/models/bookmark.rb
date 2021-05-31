333189606cfd9d0f308692c61fe01b6a7104e466
Kristoffer Duthu <kristoffer.duthu@gmail.com>
2021-05-31 22:30:12 +0800

    initial validations added, not complete yet

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+  validates :list, presence: true, uniqueness: { scope: :movie }
+  validates :movie, presence: true, uniqueness: { scope: :list }
+  validates :comment, length: { minimum: 6 }
 end
