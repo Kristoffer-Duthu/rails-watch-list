9344baf2f65adfac39acfec3587b068d69ea481c
Kristoffer Duthu <kristoffer.duthu@gmail.com>
2021-06-04 23:16:14 +0800

    added more css classes and worked on some color combos

--- app/javascript/components/index-banner.js
+++ app/javascript/components/index-banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const dynamicBannerText = () => {
   new Typed ('#banner-typed-text', {
-    strings: ["The Greatest Movie Lists!", "Your Lists! "],
+    strings: ["The Greatest Movie Lists!", "Share with your friends! "],
     typeSpeed: 40,
     loop: true
   });
