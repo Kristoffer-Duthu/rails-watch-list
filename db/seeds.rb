485bbd031ac44bc2dd7822f66dab2a2111efb56a
Kristoffer Duthu <kristoffer.duthu@gmail.com>
2021-06-05 12:12:06 +0800

    reseeded my db

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 require 'json'
 require 'open-uri'
 
+Bookmark.destroy_all
 Movie.destroy_all
 
 url = 'http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>'
@@ -17,6 +18,9 @@ p info
 results = info["results"]
 
 results.take(10).each do |movie|
-  m = Movie.create!(title: movie["title"], overview: movie["overview"], poster_url: movie["poster_path"], rating: movie["vote_average"])
+  base = "https://image.tmdb.org/t/p/w500/"
+  poster_url = movie["poster_path"].include?("https://image.tmdb.org") ? movie["poster_path"] : base + movie["poster_path"]
+
+  m = Movie.create!(title: movie["title"], overview: movie["overview"], poster_url: poster_url, rating: movie["vote_average"])
   puts "created a movie: #{m.title}!"
 end
