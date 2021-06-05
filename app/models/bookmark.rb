bb4ae63df5f84a5e8815c93c123fd5fb9a7562b2
Kristoffer Duthu <kristoffer.duthu@gmail.com>
2021-06-05 09:53:08 +0800

    debuged the delete button issue and also added a condition on the comment to not repeat if several comments

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
-  belongs_to :list, dependent: :destroy
+  belongs_to :list
   validates :list, presence: true, uniqueness: { scope: :movie }
   validates :movie, presence: true, uniqueness: { scope: :list }
   validates :comment, length: { minimum: 6 }
